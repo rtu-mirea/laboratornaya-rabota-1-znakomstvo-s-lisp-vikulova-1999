3a0d4ece0c7f0db09c17021f64b8406309d76fbb
Taha Kula <56185668+vikulova-1999@users.noreply.github.com>
2020-01-29 12:36:00 +0300

    Update ZadanieSoSpiskami.lisp

--- ZadanieSoSpiskami.lisp
+++ ZadanieSoSpiskami.lisp
@@ -1,7 +1,25 @@
-(print 
-     (cddr 
-          (list (list 8 10) (list 9 1) (car (list 2 8))  (cdr (list 3 7)) (cdr'' (list 6 4)))
-      )
- )
+
+
+; вывод изначального списка
+
+(print
+(list (list 3 1 7) 6 (list 1 3 2) (car(list 2 2 3)) (list 3 1 5)))
+
+;вывод 3 список полностью
+
+(print
+(nth 2 (list (list 3 1 7) 6 (list 1 3 2) (car(list 2 2 3)) (list 3 1 5))))
+ 
+;вывод 2-го эл-та  3-го списка
+(print
+(nth 1 (nth 2 (list (list 3 1 7) 6 (list 1 3 2) (car(list 2 2 3)) (list 3 1 5)))))
+ 
+;вывод суммы 1-го и 3-го списков
+(print
+(+ (nth 0 (list (list 3 1 7) 6 (list 1 3 2) (car(list 2 2 3)) (list 3 1 5))) (nth 2 (list (list 3 1 7) 6 (list 1 3 2) (car(list 2 2 3)) (list 3 1 5)))))
+
+; вывод 5-го элемента 1-го списка
+(print
+(nth 4 (nth 0 (list (list 3 1 7) 6 (list 1 3 2) (car(list 2 2 3)) (list 3 1 5)))))
 
 
